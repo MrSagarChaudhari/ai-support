3cf18e90fc61b2282f07981667d22eaac1d89556
Mr. Sagar Chaudhari <officialmrsagar2@gmail.com>
2025-08-16 18:04:43 +0300

    03: clerk authentication

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,20 +1,34 @@
 "use client";
 
-import { useMutation, useQuery } from "convex/react";
+import {
+  useMutation,
+  useQuery,
+  Authenticated,
+  Unauthenticated,
+} from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
+import { SignInButton, UserButton } from "@clerk/nextjs";
 import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
   const users = useQuery(api.users.getMany);
   const addUser = useMutation(api.users.add);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <Button onClick={() => addUser()}>add</Button>
-        <p>apps/web</p>
-
-        {JSON.stringify(users, null, 2)}
-      </div>
-    </div>
+    <>
+      <Authenticated>
+        <div className="flex items-center justify-center min-h-svh">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <Button onClick={() => addUser()}>add</Button>
+            <p>apps/web</p>
+            <UserButton />
+            {JSON.stringify(users, null, 2)}
+          </div>
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        <p>Must be singe in!</p>
+        <SignInButton>SineIn</SignInButton>
+      </Unauthenticated>
+    </>
   );
 }
